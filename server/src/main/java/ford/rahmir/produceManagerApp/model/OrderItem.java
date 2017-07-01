c00dfd1abbe6428d2b73922ead2a21f9d08fd7f2
Rahmir Ford <Rahmir.ford@gmail.com>
2017-07-01 17:37:00 -0400

    initial commit, server and client-side already setup

---
+++ server/src/main/java/ford/rahmir/produceManagerApp/model/OrderItem.java
@@ -0,0 +1,45 @@
+package ford.rahmir.produceManagerApp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+/**
+ * Created by rahmirford on 6/29/17.
+ */
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String productDescription;
+    private double quantity;
+
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
